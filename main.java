d2bfe5b4f3d0d902f50227b6ff7f8a945badef21
Bridget Naylor <48873843+TheCoderNamedBridget@users.noreply.github.com>
2021-05-06 15:28:11 -0700

    Update main.java

--- main.java
+++ main.java
@@ -1,20 +1,50 @@
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Map.Entry;
 import java.util.PriorityQueue;
 import java.util.Queue;
+import java.util.Scanner;
+
 
 
 
 /*
  * Todo Debug the problem that some values are not getting converted to the correct reg \
- * problem is that the program can't differentiate between 
+ * okay there is a porblem with how the/when the values are being assigned registers
+ * 
+ * 
+ * add a check to make sure that integers are not getting double registered
  */
 public class StudentSolver {
-	static boolean testingProgram = false;
-	static boolean testingProgramOld = false;
-	static boolean testingPrint = true;
+	static boolean testingProgram = true;
 	static boolean testingPrintDebugger = false;
+	static boolean testingProgramOld = false;
+	
+	
+	static boolean testingPrint = true;
+	
+	
+	public static Integer getMax(ArrayList<ArrayList<Integer>> c)
+	{
+		int max = 0;
+		for ( int i = 0; i < c.size(); i++ )
+		{
+			for ( int j = 0; j < c.get(i).size(); j++ )
+			{
+				if ( c.get(i).get(j) > max )
+				{
+					max = c.get(i).get(j);
+				}
+			}
+		}
+		return max;
+	}
+	
+	
 	public static ArrayList<ArrayList<Integer>> solve(ArrayList<ArrayList<Integer>> c)
 	{
 		if ( testingPrint )
@@ -32,61 +62,162 @@ public class StudentSolver {
 		
 		HashMap<Integer, Integer> countOfNumAppearances = new HashMap<Integer, Integer>();
 		//counts num times num appears in list
+		HashMap<Integer, Boolean> intExistsInLine = new HashMap<Integer, Boolean>();
+		int totalNumsInArray = 0;
+		int numRepeats = 0;
 		for ( int i = 0; i < c.size(); i ++ )
 		{
+			intExistsInLine.clear();
 			for ( int j = 0; j < c.get(i).size(); j ++  )
 			{
-				if ( countOfNumAppearances.get(c.get(i).get(j)) == null )
+				if ( intExistsInLine.get(c.get(i).get(j)) == null )
 				{
-					countOfNumAppearances.put(c.get(i).get(j), 1);
+					intExistsInLine.put(c.get(i).get(j), true);
 				}
 				else 
 				{
-					countOfNumAppearances.put(c.get(i).get(j), countOfNumAppearances.get(c.get(i).get(j)) + 1);
+					numRepeats++;
+				}
+				totalNumsInArray++;
+			}
+			
+			Iterator<Entry<Integer, Boolean>> overExists = intExistsInLine.entrySet().iterator();
+			while ( overExists.hasNext() )
+			{
+				Integer curInt = overExists.next().getKey();
+				if ( countOfNumAppearances.get(curInt) == null )
+				{
+					countOfNumAppearances.put(curInt, 1);
+				}
+				else 
+				{
+					countOfNumAppearances.put(curInt, countOfNumAppearances.get(curInt) + 1);
 				}
 			}
 		}
+		/*
+		 * Code listing the counts of each number in line
+		 */
+		Iterator<Entry<Integer, Integer>> ITR = countOfNumAppearances.entrySet().iterator();
+		System.out.println("List of Counts: size = " + countOfNumAppearances.size());
+		int totalNumperLine = 0;
+		while ( ITR.hasNext() )
+		{
+			Entry<Integer, Integer> itrNext = ITR.next();
+			System.out.println( "num " + itrNext.getKey() + " count:" + itrNext.getValue());
+			totalNumperLine += itrNext.getValue();
+		}
+		/*code to see how many numbers found*/
+		int total = 0;
+		Iterator<Entry<Integer, Integer>> itr = countOfNumAppearances.entrySet().iterator();
+		System.out.println("TOTALTOTAL " + totalNumsInArray  + " total num repeats " + numRepeats );
+		System.out.println("totalNumperLine " +totalNumperLine );
+		while ( itr.hasNext() )
+		{
+			Entry<Integer, Integer> i = itr.next();
+			total += i.getValue();
+			System.out.println("TOTAL Numbers in hash " + total + " for num " + i.getKey());
+		}
+		 
 		
 		
 		HashMap<Integer, Integer> regAssignedToNum = new HashMap<Integer, Integer>();
-		int unsusedRegisters = 0;
-		int curRegNum = 1;
+		int curRegNum = 0;
 		Queue<Integer> queueOfUnusedReg = new PriorityQueue<Integer>();
 		//iterates through creating, reusing, switching registers
 		if ( testingPrintDebugger )
 		{
 			System.out.println("Debugging Registers Part 1: ");
 		}
+		HashMap<Integer, Boolean> alreadyDeletedNumPerLine = new HashMap<Integer, Boolean>();
 		
 		for ( int i = 0; i < c.size(); i ++ )
 		{
-			int numNumsWithCountOne = 0;
+			alreadyDeletedNumPerLine.clear();
 			for ( int j = 0; j < c.get(i).size(); j ++  )
 			{
-				//if no reg and not already in a reg and none of numbers in list count == 1 then make reg
-				if ( regAssignedToNum.get(c.get(i).get(j)) == null && unsusedRegisters == 0 )
+				int item = c.get(i).get(j);
+				if ( alreadyDeletedNumPerLine.get(item) == null && regAssignedToNum.get(item) == null )
 				{
-					regAssignedToNum.put(c.get(i).get(j), curRegNum);
-					curRegNum++;
+					int regTouse = curRegNum;
+					if ( queueOfUnusedReg.size() != 0 )
+					{
+						regTouse = queueOfUnusedReg.poll();
+					}
+					else if ( queueOfUnusedReg.size() == 0 )
+					{
+						curRegNum++;
+					}
+					regAssignedToNum.put(item, regTouse);
+					alreadyDeletedNumPerLine.put(item, true);
+					
 				}
-				else if ( regAssignedToNum.get(c.get(i).get(j)) == null && unsusedRegisters != 0 && queueOfUnusedReg.size() != 0)
-				{
-					regAssignedToNum.put(c.get(i).get(j), queueOfUnusedReg.poll());
-					unsusedRegisters--;
-					curRegNum++;
-				}
-				//count of 
-				if ( countOfNumAppearances.get(c.get(i).get(j)) == 1 )
-				{
-					numNumsWithCountOne ++;
-					queueOfUnusedReg.add(regAssignedToNum.get(c.get(i).get(j)));
-				}
+				countOfNumAppearances.put(item, countOfNumAppearances.get(item) -1 );
 				
-				countOfNumAppearances.put(c.get(i).get(j), countOfNumAppearances.get(c.get(i).get(j)) -1 );
-			
 			}
-			unsusedRegisters += numNumsWithCountOne;
+				
 		}
+		
+		//code for old part of assigning registers
+//		for ( int i = 0; i < c.size(); i ++ )
+//		{
+//			alreadyDeletedNumPerLine.clear();
+//			for ( int j = 0; j < c.get(i).size(); j ++  )
+//			{
+//				
+//				//if no reg and not already in a reg and none of numbers in list count == 1 then make reg
+//				if ( alreadyDeletedNumPerLine.get(c.get(i).get(j)) == null && regAssignedToNum.get(c.get(i).get(j)) == null )
+//				{
+//					if (queueOfUnusedReg.size() == 0)
+//					{
+//						regAssignedToNum.put(c.get(i).get(j), curRegNum);
+//						System.out.println(" HERE : num " + c.get(i).get(j) + " cur reg " + curRegNum  
+//								+ " count left " + countOfNumAppearances.get(c.get(i).get(j)) );
+//						curRegNum++;
+//					}
+//					else
+//					{
+//						Integer reg = queueOfUnusedReg.poll();
+//						regAssignedToNum.put(c.get(i).get(j), reg);
+//						System.out.println(" HERE Reusing Old Reg : " + reg + " for num " + c.get(i).get(j) );
+//					}
+//					
+////					if ( countOfNumAppearances.get(c.get(i).get(j)) == 1 )
+////					{
+////						queueOfUnusedReg.add(regAssignedToNum.get(c.get(i).get(j)));
+////					}
+//				}
+//				
+//				//count of 
+//				//possible error here bc could be looking at a duplicate num
+//				if ( alreadyDeletedNumPerLine.get(c.get(i).get(j)) == null && countOfNumAppearances.get(c.get(i).get(j)) == 1 )//if this is the last time a number is appearing
+//				{
+//					queueOfUnusedReg.add(regAssignedToNum.get(c.get(i).get(j)));
+//					System.out.println(" HERE2 : c.get(i).get(j) = " + c.get(i).get(j) + " count = " + countOfNumAppearances.get(c.get(i).get(j)) );
+//					System.out.println(" Unused reg " + queueOfUnusedReg.size());
+//				}
+//				
+//				
+//				if ( alreadyDeletedNumPerLine.get(c.get(i).get(j)) == null )
+//				{
+//					alreadyDeletedNumPerLine.put(c.get(i).get(j), true);
+//
+//					countOfNumAppearances.put(c.get(i).get(j), countOfNumAppearances.get(c.get(i).get(j)) -1 );
+//				}
+//				
+//				if ( curRegNum > 82 )
+//				{
+//					System.out.println("Register over 82 at " + c.get(i).get(j) + " regNum " + curRegNum);
+//				}
+//			
+//			}
+			if ( testingPrintDebugger )
+			{
+				System.out.println();
+			}
+		
+		
+		System.out.println("regAssignedToNum size " + regAssignedToNum.size());
 		ArrayList<ArrayList<Integer>> reg = new ArrayList<ArrayList<Integer>>();
 		//convert the numbers to their register
 		if ( testingPrintDebugger )
@@ -102,7 +233,7 @@ public class StudentSolver {
 				placeholderForRegisters.add(regAssignedToNum.get(c.get(i).get(j)));
 				if ( testingPrintDebugger )
 				{
-					System.out.print("num: " + c.get(i).get(j) + " reg: " + regAssignedToNum.get(c.get(i).get(j)) + " ");
+					System.out.print("num: " + c.get(i).get(j) );
 				}
 				
 			}
@@ -133,59 +264,54 @@ public class StudentSolver {
 	
 	public static void main(String args[]) 
 	{
-		if ( testingProgram )
-		{
-			
 		
-			ArrayList<ArrayList<Integer>> c = new ArrayList<ArrayList<Integer>>();
-			for ( int i = 0; i < 5; i ++ )
+		System.out.println("Here");
+		ArrayList<ArrayList<Integer>> c = new ArrayList<ArrayList<Integer>>();
+		
+		try {
+		      File myObj = new File("problem.txt");
+		      Scanner myReader = new Scanner(myObj);
+		      while (myReader.hasNextLine()) {
+		        String data = myReader.nextLine();
+		        ArrayList<Integer> cPlaceholder = new ArrayList<Integer>();
+		        String placeholder = "";
+		        for ( int i = 0; i < data.length(); i ++)
+		        {
+//		        	System.out.println("here[" + data.substring(i, i + 1) + "]");
+		        	if ( !data.substring(i, i + 1).equals(" ") )
+		        	{
+		        		placeholder += data.substring(i, i + 1);
+		        	}
+		        	else 
+		        	{
+//		        		System.out.println("[" + placeholder + "]");
+		        		cPlaceholder.add(Integer.parseInt(placeholder));
+		        		placeholder = "";
+		        	}
+		        	
+		        }
+		        cPlaceholder.add(Integer.parseInt(placeholder));
+		        c.add(cPlaceholder);
+		        System.out.println(data);
+		      }
+		      myReader.close();
+		    } catch (FileNotFoundException e) {
+		      System.out.println("An error occurred.");
+		      e.printStackTrace();
+		    }
+		
+		
+		System.out.println("Testing Parser: ");
+		for ( int i = 0; i < c.size(); i ++ )
+		{
+			for ( int j = 0; j < c.get(i).size(); j ++ )
 			{
-				int ranReg = (int)(Math.random()*100);
-	//			System.out.println( ranReg);
-				ArrayList<Integer> cnew = new ArrayList<Integer>();
-				for ( int j = 0; j < 5; j ++ ) 
-				{
-					
-					if ( j == 0 )
-					{
-						cnew.add(ranReg);
-					}
-					else 
-					{
-						int ranNum = (int)(Math.random()*10);
-	//					System.out.println( ranNum);
-						cnew.add( ranNum);
-					}
-					
-				}
-				c.add(cnew);
+				System.out.print( c.get(i).get(j) + " ");
 			}
-			boolean testingPrint = false;
-			if ( testingPrint )
-			{
-				System.out.println("Print of 2D ArrayList: ");
-				for ( int i = 0; i < 100; i ++ )
-				{
-					for ( int j = 0; j < 10; j ++ ) 
-					{
-						
-						System.out.print( c.get(i).get(j) + " " );
-						
-					}
-					System.out.println();
-				}
-			}
-			
-			solve(c);
-			System.out.println("After Duplicates Removed: size = " + c.size());
-			if ( testingPrint )
-			{
-				System.out.println("After Duplicates Removed: size = " + c.size());
-				for ( int i = 0; i < c.size(); i ++ )
-				{
-					System.out.println( c.get(i).get(0));
-				}
-			}
+			System.out.println();
 		}
+		c = solve(c);
+		
+		System.out.println("max = " + getMax(c));
 	}
 }
